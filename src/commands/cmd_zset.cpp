dbe88ee0ad27cc4fde46493e781edbc35ebb4412
Aryan <aryanmehtayt2@gmail.com>
2025-12-23 16:55:00 +0000

    Added support for retrieving the rank using the ZRANK command

--- src/commands/cmd_zset.cpp
+++ src/commands/cmd_zset.cpp
@@ -1,49 +1,94 @@
 #include "cmd_zset.hpp"
 #include "../utils/utils.hpp"
 #include "../db/structs/redis_zset.hpp"
+#include <iostream>
 
 std::string ZSetCommands::handle(Database& db, const std::vector<std::string>& args) {
     std::string command = to_upper(args[0]);
     std::string response;
 
-    if (command == "ZADD" && args.size() >= 4) {
-        std::string key = args[1];
-        double score = 0;
-        std::string member = args[3];
-        bool wrong_type = false;
-        int added_count = 0;
-
-        try {
-            score = std::stod(args[2]);
-        } catch (...) {
-            return "-ERR value is not a valid float\r\n";
+    if (command == "ZADD") {
+        if (args.size() < 4 || (args.size() - 2) % 2 != 0) {
+             return "-ERR wrong number of arguments for 'zadd' command\r\n";
         }
+
+        std::string key = args[1];
+        int added_count = 0;
+        bool wrong_type = false;
+        bool error_parsing = false;
 
         {
             std::lock_guard<std::mutex> lock(db.kv_mutex);
             auto it = db.kv_store.find(key);
-
+            
             if (it == db.kv_store.end()) {
                 Entry entry;
                 entry.type = VAL_ZSET;
-                added_count = RedisZSet::add(entry, score, member);
                 db.kv_store[key] = entry;
+                it = db.kv_store.find(key);
+            }
+
+            if (it->second.type != VAL_ZSET) {
+                wrong_type = true;
             } else {
-                if (it->second.type != VAL_ZSET) {
-                    wrong_type = true;
-                } else {
-                    added_count = RedisZSet::add(it->second, score, member);
+                for (size_t i = 2; i < args.size(); i += 2) {
+                    std::string score_str = args[i];
+                    std::string member = args[i+1];
+                    double score = 0;
+                    
+                    try {
+                        score = std::stod(score_str);
+                    } catch (...) {
+                        error_parsing = true;
+                        break; 
+                    }
+
+                    added_count += RedisZSet::add(it->second, score, member);
                 }
             }
         }
 
         if (wrong_type) {
             response = "-WRONGTYPE Operation against a key holding the wrong kind of value\r\n";
+        } else if (error_parsing) {
+            response = "-ERR value is not a valid float\r\n";
         } else {
             response = ":" + std::to_string(added_count) + "\r\n";
         }
-    } else {
-        response = "-ERR wrong number of arguments for 'zadd' command\r\n";
+    }
+    else if (command == "ZRANK") {
+        if (args.size() < 3) {
+            return "-ERR wrong number of arguments for 'zrank' command\r\n";
+        }
+
+        std::string key = args[1];
+        std::string member = args[2];
+        long long rank = -1;
+        bool wrong_type = false;
+
+        {
+            std::lock_guard<std::mutex> lock(db.kv_mutex);
+            auto it = db.kv_store.find(key);
+            
+            if (it != db.kv_store.end()) {
+                if (it->second.type != VAL_ZSET) {
+                    wrong_type = true;
+                } else {
+                    rank = RedisZSet::rank(it->second, member);
+                }
+            }
+        }
+
+        if (wrong_type) {
+            response = "-WRONGTYPE Operation against a key holding the wrong kind of value\r\n";
+        } else if (rank != -1) {
+            response = ":" + std::to_string(rank) + "\r\n";
+        } else {
+            response = "$-1\r\n";
+        }
+    }
+    else {
+         response = "-ERR unknown command\r\n";
     }
     return response;
 }
--- src/db/structs/redis_zset.hpp
+++ src/db/structs/redis_zset.hpp
@@ -2,6 +2,7 @@
 #include "../object.hpp"
 #include <string>
 #include <utility>
+#include <iterator>
 
 class RedisZSet {
 public:
@@ -23,5 +24,24 @@ public:
             tree.insert({score, member});
             return 1;
         }
+    }
+
+    static long long rank(const Entry& entry, const std::string& member) {
+        const auto& dict = entry.zset_val.dict;
+        auto it_dict = dict.find(member);
+        
+        if (it_dict == dict.end()) {
+            return -1;
+        }
+
+        double score = it_dict->second;
+        const auto& tree = entry.zset_val.tree;
+ 
+        auto it_tree = tree.find({score, member});
+        if (it_tree == tree.end()) {
+            return -1;
+        }
+
+        return std::distance(tree.begin(), it_tree);
     }
 };

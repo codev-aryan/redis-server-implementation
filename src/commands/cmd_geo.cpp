1517194e3375b16127d87c15de06494e17950e3b
Aryan <aryanmehtayt2@gmail.com>
2025-12-24 11:38:37 +0000

    validating the latitude and longitude values

--- src/commands/cmd_geo.cpp
+++ src/commands/cmd_geo.cpp
@@ -1,10 +1,38 @@
 #include "cmd_geo.hpp"
 #include "../utils/utils.hpp"
+#include <string>
+#include <vector>
 
 std::string GeoCommands::handle(Database& db, const std::vector<std::string>& args) {
     std::string command = to_upper(args[0]);
 
     if (command == "GEOADD") {
+        if (args.size() < 5 || (args.size() - 2) % 3 != 0) {
+            return "-ERR wrong number of arguments for 'geoadd' command\r\n";
+        }
+
+        for (size_t i = 2; i < args.size(); i += 3) {
+            std::string long_str = args[i];
+            std::string lat_str = args[i+1];
+
+            double longitude = 0.0;
+            double latitude = 0.0;
+
+            try {
+                longitude = std::stod(long_str);
+                latitude = std::stod(lat_str);
+            } catch (...) {
+                return "-ERR value is not a valid float\r\n";
+            }
+
+            if (longitude < -180.0 || longitude > 180.0) {
+                return "-ERR invalid longitude\r\n";
+            }
+
+            if (latitude < -85.05112878 || latitude > 85.05112878) {
+                return "-ERR invalid latitude\r\n";
+            }
+        }
         return ":1\r\n";
     }
     

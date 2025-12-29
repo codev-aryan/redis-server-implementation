b1653cfa43012b80da462d6a3ab2ebc6b948744c
Aryan <aryanmehtayt2@gmail.com>
2025-12-29 10:57:59 +0000

    Added support for delivering published messages

--- src/commands/cmd_pubsub.cpp
+++ src/commands/cmd_pubsub.cpp
@@ -2,6 +2,7 @@
 #include "../server/client.hpp"
 #include "../utils/utils.hpp"
 #include <iostream>
+#include <sys/socket.h>
 
 std::string PubSubCommands::handle_subscribe(Database& db, std::shared_ptr<Client> client, const std::vector<std::string>& args) {
     if (args.size() < 2) return "-ERR wrong number of arguments for 'subscribe' command\r\n";
@@ -31,13 +32,23 @@ std::string PubSubCommands::handle_publish(Database& db, const std::vector<std::
     if (args.size() < 3) return "-ERR wrong number of arguments for 'publish' command\r\n";
 
     std::string channel = args[1];
+    std::string message = args[2];
     int subscriber_count = 0;
+
+    std::string push_msg = "*3\r\n$7\r\nmessage\r\n$" + std::to_string(channel.length()) + "\r\n" + channel + "\r\n$" + std::to_string(message.length()) + "\r\n" + message + "\r\n";
 
     {
         std::lock_guard<std::mutex> lock(db.pubsub_mutex);
         auto it = db.pubsub_channels.find(channel);
         if (it != db.pubsub_channels.end()) {
-            subscriber_count = it->second.size();
+            auto& clients = it->second;
+            subscriber_count = clients.size();
+            
+            for (auto* client : clients) {
+                if (client->fd > 0) {
+                    send(client->fd, push_msg.c_str(), push_msg.size(), 0);
+                }
+            }
         }
     }
     return ":" + std::to_string(subscriber_count) + "\r\n";

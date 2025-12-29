55d1768fefd7c07bf5c7dfbe86a0c325ee802f29
Aryan <aryanmehtayt2@gmail.com>
2025-12-29 11:04:57 +0000

    Added support for the UNSUBSCRIBE command  to unsubscribe from a channel

--- src/commands/cmd_pubsub.cpp
+++ src/commands/cmd_pubsub.cpp
@@ -3,6 +3,7 @@
 #include "../utils/utils.hpp"
 #include <iostream>
 #include <sys/socket.h>
+#include <algorithm>
 
 std::string PubSubCommands::handle_subscribe(Database& db, std::shared_ptr<Client> client, const std::vector<std::string>& args) {
     if (args.size() < 2) return "-ERR wrong number of arguments for 'subscribe' command\r\n";
@@ -28,6 +29,52 @@ std::string PubSubCommands::handle_subscribe(Database& db, std::shared_ptr<Clien
     return response;
 }
 
+std::string PubSubCommands::handle_unsubscribe(Database& db, std::shared_ptr<Client> client, const std::vector<std::string>& args) {
+    std::vector<std::string> channels_to_process;
+
+    if (args.size() > 1) {
+        for (size_t i = 1; i < args.size(); ++i) {
+            channels_to_process.push_back(args[i]);
+        }
+    } else {
+        if (client->subscriptions.empty()) {
+             return "*3\r\n$11\r\nunsubscribe\r\n$-1\r\n:0\r\n";
+        }
+        for (const auto& channel : client->subscriptions) {
+            channels_to_process.push_back(channel);
+        }
+    }
+
+    std::string response;
+
+    for (const auto& channel : channels_to_process) {
+        bool was_subscribed = false;
+        
+        auto it = client->subscriptions.find(channel);
+        if (it != client->subscriptions.end()) {
+            client->subscriptions.erase(it);
+            was_subscribed = true;
+        }
+        if (was_subscribed) {
+            std::lock_guard<std::mutex> lock(db.pubsub_mutex);
+            auto db_it = db.pubsub_channels.find(channel);
+            if (db_it != db.pubsub_channels.end()) {
+                db_it->second.erase(client.get());
+                if (db_it->second.empty()) {
+                    db.pubsub_channels.erase(db_it);
+                }
+            }
+        }
+
+        response += "*3\r\n";
+        response += "$11\r\nunsubscribe\r\n";
+        response += "$" + std::to_string(channel.length()) + "\r\n" + channel + "\r\n";
+        response += ":" + std::to_string(client->subscriptions.size()) + "\r\n";
+    }
+
+    return response;
+}
+
 std::string PubSubCommands::handle_publish(Database& db, const std::vector<std::string>& args) {
     if (args.size() < 3) return "-ERR wrong number of arguments for 'publish' command\r\n";
 
@@ -43,7 +90,6 @@ std::string PubSubCommands::handle_publish(Database& db, const std::vector<std::
         if (it != db.pubsub_channels.end()) {
             auto& clients = it->second;
             subscriber_count = clients.size();
-            
             for (auto* client : clients) {
                 if (client->fd > 0) {
                     send(client->fd, push_msg.c_str(), push_msg.size(), 0);
@@ -51,5 +97,6 @@ std::string PubSubCommands::handle_publish(Database& db, const std::vector<std::
             }
         }
     }
+
     return ":" + std::to_string(subscriber_count) + "\r\n";
 }
--- src/commands/cmd_pubsub.hpp
+++ src/commands/cmd_pubsub.hpp
@@ -9,5 +9,6 @@ class Client;
 class PubSubCommands {
 public:
     static std::string handle_subscribe(Database& db, std::shared_ptr<Client> client, const std::vector<std::string>& args);
+    static std::string handle_unsubscribe(Database& db, std::shared_ptr<Client> client, const std::vector<std::string>& args);
     static std::string handle_publish(Database& db, const std::vector<std::string>& args);
 };
